ddaa8afc2aa7f6df90c6e8b4ebd94cdfbdada592
AKSHAT SHARMA <akshatgopal70@gmail.com>
2025-01-30 22:50:41 +0530

    add Navbar component and integrate animate.css for enhanced UI effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { Button } from "./components/ui/button"
-import "./index.css"
+import { Button } from "./components/ui/button";
+import "./index.css";
+import Navbar from "./page/Navbar";
+
 function App() {
   return (
-    <>
-    <Navbar/>
-    </>
-  )
+    <div className="bg-gradient-to-r from-indigo-200 from-10% via-sky-100 via-30% to-emerald-100 to-90% h-[100vh] w-[100vw]">
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
