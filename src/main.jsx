dcab7c58834aa34cf5896d5c58f01cd79ed78760
AKSHAT SHARMA <akshatgopal70@gmail.com>
2025-01-31 17:48:29 +0530

    style: enhance scrollbar appearance and prevent horizontal overflow

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,19 @@ import App from "./App.jsx";
 import "./index.css";
 import AnimatedCursor from "react-animated-cursor";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AnimatedCursor
-      innerSize={13}
-      outerSize={13}
-      color="0, 46, 231"
-      outerStyle={{
-        border: "5px solid #20A4F7",
-        backgroundColor: "#20A4F7",
-      }}
-      innerScale={1.2}
-      outerScale={1.9}
-    />
-    <App />
+      <AnimatedCursor
+        innerSize={13}
+        outerSize={13}
+        color="0, 46, 231"
+        outerStyle={{
+          border: "5px solid #20A4F7",
+          backgroundColor: "#20A4F7",
+        }}
+        innerScale={1.2}
+        outerScale={1.9}
+      />
+      <App />
   </StrictMode>
 );
