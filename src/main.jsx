66900fb2ee04864f0b34873b4449393310ea83f1
AKSHAT SHARMA <akshatgopal70@gmail.com>
2025-01-31 17:20:03 +0530

    add react-animated-cursor for enhanced user interaction and visual feedback

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "./index.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import AnimatedCursor from "react-animated-cursor";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <AnimatedCursor
+      innerSize={13}
+      outerSize={13}
+      color="0, 46, 231"
+      outerStyle={{
+        border: "5px solid #20A4F7",
+        backgroundColor: "#20A4F7",
+      }}
+      innerScale={1.2}
+      outerScale={1.9}
+    />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
